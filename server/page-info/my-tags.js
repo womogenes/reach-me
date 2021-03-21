a8d1989c6eef563b10eb981b320dcc485a5c06e1
William Y. Feng <williamf24@lakesideschool.org>
2021-03-20 19:50:26 -0700

    Add grade calculation

--- server/page-info/my-tags.js
+++ server/page-info/my-tags.js
@@ -1,15 +1,15 @@
 module.exports = async (userID, userdb) => {
-    const user = await userdb.model('Tags').findOne({ userID: userID });
+  const user = await userdb.model('Tags').findOne({ userID: userID });
 
-    if (!user) return [];
-      
-    const toReturn = await Promise.all(user.tags.map(async tagID => {
-      const tag = await userdb.model('ValidTag').findOne({ _id: tagID });
-      return {
-        name: tag.name,
-        category: tag.category
-      };
-    }));
+  if (!user) return [];
     
-    return toReturn;
+  const toReturn = await Promise.all(user.tags.map(async tagID => {
+    const tag = await userdb.model('ValidTag').findOne({ _id: tagID });
+    return {
+      name: tag.name,
+      category: tag.category
+    };
+  }));
+  
+  return toReturn;
 };
--- server/pages/dashboard.js
+++ server/pages/dashboard.js
@@ -8,18 +8,23 @@ module.exports = ({ app, userdb }) => {
     const { userID } = req.session;
 
     const userInfo = await myInfo(userID, userdb);
-    if (userInfo === 500) { res.sendStatus(500); return; }
+    if (userInfo === 500) { res.sendStatus(500); return; } // Oh no! User doesn't exis
 
     const userBio = await myBio(userID, userdb);
 
     const userTags = await myTags(userID, userdb);
+
+    const ampIndex = userInfo.email.indexOf('@');
+    const grade = 12 - (userInfo.email.substring(ampIndex - 2, ampIndex))
+                    + new Date().getFullYear() - 2000; // Thing to compute grade based on graduation year and current year
 
     res.render('dashboard.ejs', {
       name: userInfo.name,
       email: userInfo.email,
       picture: userInfo.picture,
       bio: userBio,
-      tags: userTags
+      tags: userTags,
+      grade: grade
     });
   });
 };
